1a914d3bd69559bfc10121b991d240bc6f0ef79e
SoMin <thals0107@naver.com>
2022-08-26 12:54:13 +0900

    error 해결

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(8080, function () {
